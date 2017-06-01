4fde41e3331e8a984b2d518600b189fc4dc7b708
valyke <markganzonlalata@gmail.com>
2017-06-01 17:23:12 +0800

    Add User Model

--- model/user.js
+++ model/user.js
@@ -1,9 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var bcrypt = require('bcrypt-nodejs');
+
 var userSchema = new Schema({
 	email: { type: String, required: true },
 	password: { type: String, required: true }
 });
 
-module.exports = moongoose.model('User', userSchema);
+
+userSchema.methods.encryptPassword = function(password) {
+	return bcrypt.hashSync(password);
+};
+
+userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
