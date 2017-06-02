449655df1bd29033b856b9edd1f6307b1a3d117b
valyke <markganzonlalata@gmail.com>
2017-06-02 13:49:44 +0800

    Display products from database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var Product = require('../model/product');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', {});
+	Product.find({}, function(err, product) {
+  		res.render('shop/index', { products: product });
+	});
 });
 
 module.exports = router;
